09abf49589dad70ba351c8106ccfb257862580f2
josh10e <j10e4077@gmail.com>
2026-02-12 16:47:21 -0700

    Filled out app.jsx following same steps as with the simon tutorial

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,45 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { Home } from './home/home';
+import { DivePlanner } from './diveplanner/diveplanner';
+import { DiveLog } from './divelog/divelog';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <BrowserRouter>
+      <div className="app-body">
+        <header>
+          <h1>Dive Planner</h1>
+        </header>
+
+        <nav className="sidebar">
+          <h2>Pages</h2>
+          <ul>
+            <li>
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/diveplanner">Plan a Dive</NavLink>
+            </li>
+            <li>
+              <NavLink className="nav-link" to="/divelog">Log a Dive</NavLink>
+            </li>
+            <li>
+              <a className="nav-link" href="https://github.com/josh10e/startup">Startup Repository</a>
+            </li>
+          </ul>
+        </nav>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/diveplanner" element={<DivePlanner />} />
+            <Route path="/divelog" element={<DiveLog />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
