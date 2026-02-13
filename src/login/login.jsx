08c2c90b07ae5864b98dc22194e572885fc4afeb
josh10e <j10e4077@gmail.com>
2026-02-12 17:03:21 -0700

    Created the following files

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+    <nav className="sidebar">
+        <h2>Pages</h2>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/diveplanner">Plan a Dive</a></li>
+          <li><a href="/divelog">Log a Dive</a></li>
+          <li><a href="https://github.com/josh10e/startup">Startup Repository</a></li>
+        </ul>
+      </nav>
+
+      <section className="content">
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="username">Username:</label><br />
+          <input type="text" id="username" name="username" /><br /><br />
+
+          <label htmlFor="password">Password:</label><br />
+          <input type="password" id="password" name="password" /><br /><br />
+
+          <button type="submit">Login</button>
+        </form>
+      </section>
+  </main>
   );
 }
