64842416773c298b552be97293bfe2346f413e0d
josh10e <j10e4077@gmail.com>
2026-03-13 18:12:08 -0600

    Added static middleware to server.js so Express will work in production

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 const authCookieName = 'token';
 const port = 4000;
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 let users = [];
 let dives = [];
 
