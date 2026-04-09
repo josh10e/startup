7a533139392b905b340f2994a4c28e43eaa2ccc6
josh10e <j10e4077@gmail.com>
2026-04-08 18:41:54 -0600

    Changed vite.config.js to allow websocket

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:4000'
+      '/api': 'http://localhost:4000',
+      '/ws': {
+        target: 'ws://localhost:4000',
+        ws: true
+      }
     }
   }
 });
